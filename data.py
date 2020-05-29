6a726406945711b5c240ee20de930534b6eae982
Arudjreis <Ayoub.Fandi@fr.ey.com>
2020-05-29 18:09:24 +0200

    creating data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+# We are playing with data types
+
+#String
+nickname = "Aaron"
+
+#Integer
+money_in_checking = 14
+
+#Float
+money_in_savings = 3.50
+
+#Boolean
+Am_I_Broke = True
+
+print(nickname)
+print(money_in_checking)
+print(money_in_savings)
+print(Am_I_Broke)
