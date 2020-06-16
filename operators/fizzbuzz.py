88e8bf901f311429579a1d035aecb3899acc7b20
Arudjreis <Ayoub.Fandi@fr.ey.com>
2020-06-16 12:20:50 +0200

    functions

--- operators/fizzbuzz.py
+++ operators/fizzbuzz.py
@@ -1,11 +1,11 @@
 # Print out numbers 1 to 100
 # For numbers evenly divisible by 3, print "Fizz" instead
 # For numbers evenly divisible by 5, print "Buzz" instead
-# For numbers evenly divisible by 15, print "FIzzBuzz" instead
+# For numbers evenly divisible by 3 and 5, print "FIzzBuzz" instead
 
 for number in range(101):
 
-    if (number % 15 == 0):
+    if (number % 3 == 0 and number % 5 == 0):
         print("FizzBuzz")
     elif (number % 3 == 0):
         print("Fizz")
